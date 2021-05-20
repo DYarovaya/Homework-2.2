b64c1077a1e5bc856096de453ad5c8570e07f722
Darya Yarovaya <yarovayadarya@mail.ru>
2021-05-20 22:31:58 +0300

    Initial Commit

---
+++ Homework_2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_2.2
+//
+//  Created by Дарья Яровая on 20.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
